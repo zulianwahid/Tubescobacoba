8b83749b05a7e27d77123e260d7f1e772234e8f4
zulianwahid <zulianwahid@gmail.com>
2023-05-23 17:00:46 +0700

    update

--- app/src/main/java/com/example/tubescobacoba/splashscreen.kt
+++ app/src/main/java/com/example/tubescobacoba/splashscreen.kt
@@ -5,13 +5,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-
+import android.view.WindowManager
+import androidx.appcompat.app.WindowDecorActionBar
 
 
 class splashscreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
         setContentView(R.layout.activity_splashscreen)
         supportActionBar?.hide()
 
